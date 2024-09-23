abccade5532aecc92787af166f528427a97eca67
hakasapl <hakan@sapl.me>
2024-09-23 18:47:59 -0400

    finished plugin init mapping

--- ParallaxGenLib/include/ParallaxGenPlugin.hpp
+++ ParallaxGenLib/include/ParallaxGenPlugin.hpp
@@ -1,18 +1,62 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "BethesdaGame.hpp"
+#include "ParallaxGenTask.hpp"
 #include "XEditLibCpp.hpp"
 
 #define PLUGIN_LOADER_CHECK_INTERVAL 100
+#define HASH_GOLDENRATIO 0x9e3779b9
+#define HASH_LEFTWISE 6
+#define HASH_RIGHTWISE 2
+
+#define FORMID_BASE 16
 
 class ParallaxGenPlugin : public XEditLibCpp {
 private:
   std::vector<std::wstring> ActivePlugins;
 
+  // A helper to combine hash values
+  template <typename T> static void hashCombine(std::size_t &Seed, const T &Value) {
+    std::hash<T> Hasher;
+    Seed ^= Hasher(Value) + HASH_GOLDENRATIO + (Seed << HASH_LEFTWISE) + (Seed >> HASH_RIGHTWISE);
+  }
+
+  struct TXSTRefID {
+    std::wstring NIFPath;
+    std::wstring Name3D;
+    int Index3D;
+
+    // Equality operator (needed for std::unordered_map)
+    auto operator==(const TXSTRefID &Other) const -> bool {
+      return NIFPath == Other.NIFPath && Name3D == Other.Name3D && Index3D == Other.Index3D;
+    }
+  };
+
+  struct TXSTRefIDHash {
+    auto operator()(const TXSTRefID &Object) const -> size_t {
+      std::size_t Seed = 0;
+
+      // Hash each element of the tuple and combine them
+      hashCombine(Seed, Object.NIFPath);   // Hash the first wstring
+      hashCombine(Seed, Object.Name3D); // Hash the second wstring
+      hashCombine(Seed, Object.Index3D);   // Hash the int
+
+      return Seed;
+    }
+  };
+
+  std::unordered_map<TXSTRefID, std::unordered_set<unsigned int>, TXSTRefIDHash> TXSTRefsMap;
+
 public:
   ParallaxGenPlugin(const BethesdaGame &Game);
 
-  void init();
+  auto init() -> ParallaxGenTask::PGResult;
+
+private:
+  void addAlternateTexturesToMap(const unsigned int &Id, const std::wstring &MDLKey, const std::wstring &AltTXSTKey);
 };
--- ParallaxGenLib/src/ParallaxGenPlugin.cpp
+++ ParallaxGenLib/src/ParallaxGenPlugin.cpp
@@ -1,11 +1,12 @@
 #include "ParallaxGenPlugin.hpp"
 #include "BethesdaGame.hpp"
+#include "ParallaxGenTask.hpp"
 #include "XEditLibCpp.hpp"
 
 #include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
 #include <spdlog/spdlog.h>
 #include <windows.h>
-
 
 using namespace std;
 
@@ -35,7 +36,7 @@ ParallaxGenPlugin::ParallaxGenPlugin(const BethesdaGame &Game)
   ActivePlugins = getActivePlugins();
 }
 
-void ParallaxGenPlugin::init() {
+auto ParallaxGenPlugin::init() -> ParallaxGenTask::PGResult {
   // Load Plugins
   loadPlugins(ActivePlugins, false);
 
@@ -45,5 +46,87 @@ void ParallaxGenPlugin::init() {
     Sleep(PLUGIN_LOADER_CHECK_INTERVAL);
   }
 
-  spdlog::info("Plugin Loader is done");
+  if (LoaderStatus == XEditLibCpp::LoaderStates::IsError) {
+    spdlog::critical("Plugin loading failed");
+    return ParallaxGenTask::PGResult::FAILURE;
+  }
+
+  // Build references
+  buildReferences(0, true);
+
+  // Get TXST objects
+  auto TXSTs = getRecords(0, L"TXST", true);
+
+  // Find references to each TXST
+  for (const auto &TXST : TXSTs) {
+    auto TXSTFormID = getFormID(TXST, false);
+    spdlog::trace("Plugin Patching Init | Found Texture Set | {}", TXSTFormID);
+
+    // Find references to the TXST
+    auto CurTXSTRefs = getReferencedBy(TXST);
+    // loop through each reference
+    for (const auto &Ref : CurTXSTRefs) {
+      // Get the form id of the reference
+      auto FormID = getFormID(Ref, false);
+      spdlog::trace("Plugin Patching Init | Found Reference | {} | {}", TXSTFormID, FormID);
+
+      // Check for alternate textures for each type
+      if (hasElement(Ref, L"MODL") && hasElement(Ref, L"MODL\\MODS")) {
+        addAlternateTexturesToMap(getElement(Ref, L"MODL"), L"MODL", L"MODS");
+      }
+
+      if (hasElement(Ref, L"MOD2") && hasElement(Ref, L"MOD2\\MO2S")) {
+        addAlternateTexturesToMap(getElement(Ref, L"MOD2"), L"MOD2", L"MO2S");
+      }
+
+      if (hasElement(Ref, L"MOD3") && hasElement(Ref, L"MOD3\\MO3S")) {
+        addAlternateTexturesToMap(getElement(Ref, L"MOD3"), L"MOD3", L"MO3S");
+      }
+
+      if (hasElement(Ref, L"MOD4") && hasElement(Ref, L"MOD4\\MO4S")) {
+        addAlternateTexturesToMap(getElement(Ref, L"MOD4"), L"MOD4", L"MO4S");
+      }
+
+      if (hasElement(Ref, L"MOD5") && hasElement(Ref, L"MOD5\\MO5S")) {
+        addAlternateTexturesToMap(getElement(Ref, L"MOD5"), L"MOD5", L"MO5S");
+      }
+    }
+  }
+
+  return ParallaxGenTask::PGResult::SUCCESS;
+}
+
+void ParallaxGenPlugin::addAlternateTexturesToMap(const unsigned int &Id, const std::wstring &MDLKey, const std::wstring &AltTXSTKey) {
+  // Get the NIF name
+  auto NIFName = getValue(Id, MDLKey);
+  boost::to_lower(NIFName);  // Lowercase nif name
+
+  // Loop through alternate textures
+  auto AltTXSTs = getElements(Id, AltTXSTKey, false, false);
+  for (const auto &AltTXST : AltTXSTs) {
+    // Get 3D Name
+    auto Name3D = getValue(AltTXST, L"3D Name");
+
+    // Get New TXST
+    auto NewTXSTRaw = getValue(AltTXST, L"New Texture");
+
+    static boost::wregex TXSTRegex(L"\\[TXST:([0-9A-Fa-f]+)\\]");
+    boost::wsmatch Matches;
+
+    unsigned int TXSTFormID = 0;
+    if (boost::regex_search(NewTXSTRaw, Matches, TXSTRegex)) {
+      TXSTFormID = std::stoul(Matches[1], nullptr, FORMID_BASE);
+    } else {
+      return;
+    }
+
+    auto NewTXST = getRecord(0, TXSTFormID, true);
+
+    // Get 3D Index
+    auto Index3D = getIntValue(AltTXST, L"3D Index");
+
+    // Create Key Val
+    TXSTRefID KeyVal = {NIFName, Name3D, Index3D};
+    TXSTRefsMap[KeyVal].insert(NewTXST);
+  }
 }
